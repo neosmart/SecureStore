d9cfef8740d925146e52a64b0d0aad1e0966b6ed
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-08-28 17:06:27 -0500

    Added VaultVersion field for future use
    To be used at a later date to check whether or not the vault needs to be
    upgraded or use a different format.

--- SecureStore/Vault.cs
+++ SecureStore/Vault.cs
@@ -5,6 +5,7 @@ namespace NeoSmart.SecureStore
 {
     sealed internal class Vault
     {
+        string VaultVersion = "1.0";
         public byte[] IV;
         public SortedDictionary<string, EncryptedBlob> Data;
     }
