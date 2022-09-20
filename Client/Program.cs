6353eb563f88029e45bd6a52a01e62641a67815f
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2022-09-20 12:52:35 -0500

    Automatically add ignore rule for generated key files
    If a generated/exported key file is found to be in a directory under VCS
    control, generate an ignore file in the same directory specifying that the key
    file should not be committed to VCS.
    
    This protects against users accidentally committing their keys to the repo and
    is in line with the recent updates to the rust-powered `ssclient` release.

--- Client/Program.cs
+++ Client/Program.cs
@@ -6,11 +6,18 @@ using Mono.Options;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using System.IO;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
 
 namespace NeoSmart.SecureStore.Client
 {
     static class Program
     {
+        /// <summary>
+        /// The maximum number of levels to test when checking if a path is under VCS control.
+        /// </summary>
+        const int MAX_VCS_CHECK_DEPTH = 48;
+
         static readonly Assembly Assembly = typeof(Program).GetTypeInfo().Assembly;
         static string AssemblyVersion
         {
@@ -54,7 +61,107 @@ namespace NeoSmart.SecureStore.Client
             Delete,
         }
 
-        static void Main(string[] mainArgs)
+        enum VcsType
+        {
+            None,
+            Git,
+        }
+
+        /// <summary>
+        /// Checks the path and its parent directories to determine if it is under VCS control.
+        /// </summary>
+        /// <param name="path">The path to check if managed by VCS.</param>
+        /// <returns>The <see cref="VcsType"/> managing the <paramref name="path"/>, or <see cref="VcsType.None"/>.</returns>
+        private static VcsType GetVcsType(string path, int maxParents = MAX_VCS_CHECK_DEPTH)
+        {
+            var vcsPatterns = new[] { (Vcs: VcsType.Git, Path: ".git") };
+
+            var parent = Path.GetDirectoryName(Path.GetFullPath(path));
+            for (int i = 0; !string.IsNullOrEmpty(parent) && i < maxParents; ++i)
+            {
+                foreach (var vcs in vcsPatterns)
+                {
+                    var vcsPath = Path.Join(parent, vcs.Path);
+                    if (Directory.Exists(vcsPath))
+                    {
+                        return vcs.Vcs;
+                    }
+                }
+
+                parent = Path.GetDirectoryName(parent);
+            }
+
+            return VcsType.None;
+        }
+
+        /// <summary>
+        /// Add's a VCS ignore rule for the path <paramref name="path"/> in the ignore file <paramref name="ignoreFile"/>
+        /// according to the conventions of the VCS <paramref name="vcs"/>.
+        /// </summary>
+        /// <param name="vcs"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private async static Task<bool> AddIgnoreRuleAsync(VcsType vcs, string path, string ignoreFileDir)
+        {
+            var ignoreFile = vcs switch
+            {
+                VcsType.Git => Path.Combine(ignoreFileDir, ".gitignore"),
+                _ => throw new NotImplementedException("Support for this VCS is not yet implemented!"),
+            };
+
+            if (Path.GetDirectoryName(ignoreFile) != Path.GetDirectoryName(path))
+            {
+                throw new InvalidOperationException("Only ignore files in the same directory as the path to ignore are currently supported!");
+            }
+            if (!File.GetAttributes(path).HasFlag(FileAttributes.Normal))
+            {
+                throw new InvalidOperationException($"The file to ignore \"{path}\" is not a regular file!");
+            }
+            if (File.Exists(ignoreFile) && !File.GetAttributes(ignoreFile).HasFlag(FileAttributes.Normal))
+            {
+                throw new InvalidOperationException($"The ignore file \"{ignoreFile}\" exists and is not a regular file!");
+            }
+
+            var ignoreFileStatus = "existing";
+            var filename = Path.GetFileName(path);
+            var extension = Path.GetExtension(path);
+
+            if (!File.Exists(ignoreFile))
+            {
+                File.Create(ignoreFile).Dispose();
+                ignoreFileStatus = "newly-created";
+            }
+
+            // This is a valid exclude rule even if extension is empty
+            var wildcardExclude = $"*{extension}";
+
+            using (var reader = new StreamReader(File.Open(ignoreFile, FileMode.Open, FileAccess.Read, FileShare.Read)))
+            {
+                string? line;
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    // Since we are excluding a file in the same directory, both /foo and foo will match
+                    line = line.TrimStart('/').TrimEnd();
+                    if (line == filename || line == wildcardExclude)
+                    {
+                        // The path is already excluded
+                        return true;
+                    }
+                }
+            }
+
+            using (var writer = new StreamWriter(File.Open(ignoreFile, FileMode.Append, FileAccess.Write, FileShare.None)))
+            {
+                await writer.WriteLineAsync("# SecureStore key file ignore rule:");
+                await writer.WriteLineAsync($"/{filename}");
+            }
+
+            await Console.Error.WriteLineAsync($"Excluding key file in {ignoreFileStatus} VCS ignore file {ignoreFile}");
+
+            return true;
+        }
+
+        static async Task Main(string[] mainArgs)
         {
             // Tweak the default VaultVersionPolicy to allow upgrades when the CLI is used to interface
             // with the vault (by default, silent upgrades across major vault versions are prohibited to
@@ -65,11 +172,11 @@ namespace NeoSmart.SecureStore.Client
 
             bool help = false;
             bool version = false;
-            string path = null;
-            string password = null;
-            string keyfile = null;
-            string secretName = null;
-            string secretValue = null;
+            string? path = null;
+            string? password = null;
+            string? keyfile = null;
+            string? secretName = null;
+            string? secretValue = null;
             bool decryptAll = false;
             DecryptFormat format = DecryptFormat.None;
 
@@ -170,7 +277,7 @@ namespace NeoSmart.SecureStore.Client
                 Environment.Exit(1);
             }
 
-            string command = args[commandIndex];
+            string command = args[commandIndex].ToLower();
             args.RemoveAt(commandIndex);
             OptionSet parseOptions = options[command];
 
@@ -246,7 +353,7 @@ namespace NeoSmart.SecureStore.Client
                     else
                     {
                         Console.Error.WriteLine("Expected a single \"key=value\" or \"key\" \"value\" to set!");
-                        Environment.Exit(1);
+                        throw new ExitCodeException(1);
                     }
                 }
 
@@ -305,7 +412,7 @@ namespace NeoSmart.SecureStore.Client
                     }
                 }
 
-                SecretsManager sman = null;
+                SecretsManager? sman = null;
 
                 // Handle parameters specific to certain commands
                 if (command == "create")
@@ -343,6 +450,7 @@ namespace NeoSmart.SecureStore.Client
                         sman.LoadKeyFromPassword(password);
                     }
 
+                    bool keyCreated = false;
                     if (command == "create")
                     {
                         if (File.Exists(path) && new FileInfo(path).Length > 0)
@@ -359,16 +467,27 @@ namespace NeoSmart.SecureStore.Client
                             {
                                 sman.GenerateKey();
                                 sman.ExportKey(keyfile);
+                                keyCreated = true;
                             }
                         }
                         else if (!string.IsNullOrEmpty(keyfile))
                         {
                             sman.ExportKey(keyfile);
+                            keyCreated = true;
                         }
                     }
                     else if (password == null && keyfile != null)
                     {
                         sman.LoadKeyFromFile(keyfile);
+                    }
+
+                    if (keyCreated)
+                    {
+                        var vcsType = GetVcsType(keyfile!);
+                        if (vcsType != VcsType.None)
+                        {
+                            await AddIgnoreRuleAsync(vcsType, keyfile!, Path.GetDirectoryName(keyfile)!);
+                        }
                     }
 
                     var client = new Client(sman);
@@ -379,9 +498,23 @@ namespace NeoSmart.SecureStore.Client
                             client.Create();
                             break;
                         case "delete":
+                            if (string.IsNullOrEmpty(secretName))
+                            {
+                                Help(Console.Error, "The name of the secret to delete is required", command, parseOptions);
+                            }
+
                             client.Delete(secretName);
                             break;
                         case "set":
+                            if (string.IsNullOrEmpty(secretName))
+                            {
+                                Help(Console.Error, "The name of the secret to set or update is required", command, parseOptions);
+                            }
+                            if (string.IsNullOrEmpty(secretValue))
+                            {
+                                Help(Console.Error, "The value of the secret to set or update is required", command, parseOptions);
+                            }
+
                             client.Update(secretName, secretValue);
                             break;
                         case "get":
@@ -391,6 +524,11 @@ namespace NeoSmart.SecureStore.Client
                                 {
                                     Help(Console.Error, "--format can only be used in conjunction with --all!",
                                         command, parseOptions);
+                                }
+
+                                if (string.IsNullOrEmpty(secretName))
+                                {
+                                    Help(Console.Error, "The name of the secret to retrieve is required", command, parseOptions);
                                 }
 
                                 client.Decrypt(secretName);
@@ -429,10 +567,11 @@ namespace NeoSmart.SecureStore.Client
             Environment.Exit(exitCode);
         }
 
-        static void Help(TextWriter output, string message, string command, OptionSet options)
+        [DoesNotReturn]
+        static void Help(TextWriter output, string message, string? command = null, OptionSet? options = null)
         {
             output.WriteLine($"Error: {message}");
-            if (options != null)
+            if (command != null && options != null)
             {
                 output.WriteLine();
                 PrintOptions(output, command, options);
@@ -440,7 +579,7 @@ namespace NeoSmart.SecureStore.Client
             throw new ExitCodeException(1);
         }
 
-        static void PrintOptions(TextWriter output, string cmd, OptionSet options)
+        static void PrintOptions(TextWriter output, string? cmd, OptionSet options)
         {
             if (cmd != null)
             {
@@ -457,7 +596,7 @@ namespace NeoSmart.SecureStore.Client
             while (true)
             {
                 Console.Error.Write(message);
-                switch (Console.ReadLine().ToLower())
+                switch (Console.ReadLine()?.ToLower())
                 {
                     case "yes": return;
                     case "no": throw new ExitCodeException(1);
@@ -492,6 +631,14 @@ namespace NeoSmart.SecureStore.Client
             }
             Console.WriteLine();
             return password.ToString();
+        }
+
+        public static void ExitIfNullOrEmpty([NotNull] this string? s, string message)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                Help(Console.Error, message);
+            }
         }
     }
 }
